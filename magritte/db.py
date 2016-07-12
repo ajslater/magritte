9a258935ec7cd6edf89505c6e916cfcb8095f56a
AJ Slater <aj@slater.net>
2016-07-11 22:47:15 -0700

    use columns and simplify row->dict conversion

--- magritte/db.py
+++ magritte/db.py
@@ -7,23 +7,11 @@ from magritte.settings import Settings
 EXCLUDE_FOLDER_NAMES = ('Trash', 'TopLevelBooks', 'TopLevelKeepsakes',
                         'TopLevelLightTables', 'Projects',
                         'TopLevelWebProjects', 'TopLevelSlideshows')
-# TODO: trim keys in select
-FOLDER_KEYS = ('modelId', 'folderPath', 'folderType', 'parentFolderUuid',
-               'name', 'uuid')
-ALBUM_KEYS = ('modelId', 'name', 'uuid', 'folderUuid')
-MASTER_KEYS = ('uuid', 'fileName', 'projectUuid', 'imagePath')
-
-
-def row_to_dict(row, keys):
-    row_dict = {}
-    for key in keys:
-        row_dict[key] = row[key]
-
-    return row_dict
 
 
 def get_folders(cursor):
-    sql = 'SELECT * FROM RKFolder WHERE name IS NOT NULL ' \
+    sql = 'SELECT modelId, folderPath, parentFolderUuid, name, uuid ' \
+          'FROM RKFolder WHERE name IS NOT NULL ' \
           'AND NOT isInTrash AND folderType <> 2 '
     sql += 'AND name NOT IN %s' % (EXCLUDE_FOLDER_NAMES,)
     rows = cursor.execute(sql)
@@ -31,7 +19,7 @@ def get_folders(cursor):
     folders_by_model = {}
     folders_by_uuid = {}
     for row in rows:
-        row_dict = row_to_dict(row, FOLDER_KEYS)
+        row_dict = dict(row)
         folders_by_model[row['modelId']] = row_dict
         folders_by_uuid[row['uuid']] = row_dict
 
@@ -41,26 +29,27 @@ def get_folders(cursor):
 def get_albums(cursor, folder_uuids):
     albums = {}
 
-    sql = 'SELECT * FROM RKAlbum WHERE name IS NOT NULL AND NOT isInTrash '
+    sql = 'SELECT modelId, name, folderUuid FROM RKAlbum ' \
+          'WHERE name IS NOT NULL ' \
+          'AND NOT isInTrash '
     sql += 'AND folderUuid IN %s' % (tuple(folder_uuids),)
     rows = cursor.execute(sql)
     for row in rows:
-        albums[row['modelId']] = row_to_dict(row, ALBUM_KEYS)
+        albums[row['modelId']] = dict(row)
     return albums
 
 
 def fill_albums(cursor, albums):
     for album in albums.values():
         album_id = album.get('modelId')
-        rows = cursor.execute(
-            'SELECT * FROM RKMaster WHERE modelId IN'
-            '(SELECT masterId FROM RKVersion WHERE modelId IN '
-            '(SELECT versionId FROM RKAlbumVersion '
-            'WHERE albumId = %s))' % album_id)
+        sql = 'SELECT imagePath, fileName FROM RKMaster WHERE modelId IN' \
+              '(SELECT masterId FROM RKVersion WHERE modelId IN ' \
+              '(SELECT versionId FROM RKAlbumVersion '
+        sql += 'WHERE albumId = %s))' % album_id
+        rows = cursor.execute(sql)
         album['media'] = []
         for row in rows:
-            media = row_to_dict(row, MASTER_KEYS)
-            album['media'].append(media)
+            album['media'].append(dict(row))
 
 
 def get_conn():
@@ -73,7 +62,6 @@ def get_conn():
 
 
 def load_data():
-    # TODO with style?
     conn = get_conn()
     cursor = conn.cursor()
 
