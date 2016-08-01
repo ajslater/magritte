38f165b1bd6336f0be7c21d7e4564db97934fc4a
AJ Slater <aj@slater.net>
2016-07-31 23:52:49 -0700

    lint

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Setup file for Magritte
+"""Setup file for Magritte.
+
 Reference:
 https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
 """
@@ -9,7 +10,6 @@ import sys
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-__version__ = "1.3.1"
 README_FILENAME = "README.rst"
 REQUIREMENTS = {
     'prod': "requirements.txt",
@@ -18,20 +18,20 @@ REQUIREMENTS = {
 
 
 def get_version(package):
-    """ Return package version as listed in `__version__` in `init.py`. """
+    """Return package version as listed in `__version__` in `init.py`."""
     with open(os.path.join(package, '__init__.py'), 'rb') as init_py:
         src = init_py.read().decode('utf-8')
         return re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
 
 
 def parse_reqs(filename):
-    """ parse setup requirements from a requirements.txt file """
+    """Parse setup requirements from a requirements.txt file."""
     install_reqs = parse_requirements(filename, session=False)
     return [str(ir.req) for ir in install_reqs]
 
 
 def get_req_list():
-    """ get the requirements by weather we're building develop or not"""
+    """Get the requirements by weather we're building develop or not."""
     req_list = parse_reqs(REQUIREMENTS['prod'])
     if len(sys.argv) > 2 and sys.argv[2] == ('develop'):
         req_list += parse_reqs(REQUIREMENTS['dev'])
